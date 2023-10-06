4ac3647f62a252fa16051f8df953759524485c9e
reovdv <remco_overvelde@msn.com>
2023-10-06 10:40:40 +0200

    New script that replaces the IDs of 'FORA' with the correct IDs used in URLs

---
+++ main.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+
+#Read in the 'ArchiMate Model Exchange File Format' file
+#These files are often used to exchange reference architectures
+def read_XML_and_replace_ID_and_write_to_XML(filename):
+    soup = BeautifulSoup(filename, "xml")
+
+    element_tags = soup.find_all('element')
+
+    for element_tag in element_tags:
+        identifier = element_tag.get('identifier')
+
+        property_tags_with_propid_8 = element_tag.find('property', {"propertyDefinitionRef" : "propid-8"})
+        
+        correct_ID = property_tags_with_propid_8.find('value').text
+
+        element_tag['identifier'] = correct_ID
+
+    return soup.prettify()
+
+with open('referentiearchitecturen_xml/FORA.xml', encoding='utf-8') as f:
+        file = f.read()
+
+updated_xml = read_XML_and_replace_ID_and_write_to_XML(file)
+
+with open('FORA_veranderd.xml', 'w', encoding='utf-8') as f:
+    f.write(updated_xml)
